79e9ef13b0b24e3d0bb35b9461c4bbefcf6a3599
sverweij <sverweij@users.noreply.github.com>
2015-04-29 21:29:51 +0200

    add (empty) unit test frame for be modules

---
+++ src/script/test/t_exercise.js
@@ -0,0 +1,26 @@
+var assert = require("assert");
+var ex = require("../be/exercise");
+
+describe('exercise', function() {
+    describe('#anExerciseFunction', function() {
+        xit('should test something', function() {
+            assert.equal("found", "expected");
+        });
+    });
+});
+/*
+ This file is part of ShoutingCoach.
+
+ ShoutingCoach is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ WordyWordy is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with WordyWordy.  If not, see <http://www.gnu.org/licenses/>.
+*/
